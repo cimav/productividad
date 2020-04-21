2e543e5e5358ba0275d34191d04b3121e2f3e258
Jonathan Hernández <jonathan.hernandez@gmail.com>
2020-04-21 12:50:12 -0600

    Ordenar archivos

--- app/javascript/packs/project_documents.js
+++ app/javascript/packs/project_documents.js
@@ -1,5 +1,10 @@
 import Bulma from '@vizuaalog/bulmajs';
 
+$(document).on('change', '#documents-order', function() {
+  window.location = window.location.href.split(/[?#]/)[0] + '?orden=' + $(this).val();
+});
+
+
 $(document).on('click', '#add-new-folder', function() {
   var url = $(this).data('url');
   $.get(url, function(data) {
