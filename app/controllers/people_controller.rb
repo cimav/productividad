9501a22945efff3a8d0f505f47cb02f8d1437cdc
Jonathan Hernández <jonathan.hernandez@gmail.com>
2020-04-23 01:20:59 -0600

    s/people/person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -69,6 +69,6 @@ class PeopleController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def person_params
-      params.require(:person).permit(:first_name, :last_name, :email, :description, :start_date, :end_date, :reload, :status, :organization_id, :department_id, :people_id)
+      params.require(:person).permit(:first_name, :last_name, :email, :description, :start_date, :end_date, :status, :organization_id, :department_id, :person_id)
     end
 end
