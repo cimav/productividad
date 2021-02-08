fea25c1aea7d9ec3d6a00901117f70a55b1593d0
Jonathan Hernández <jonathan.hernandez@gmail.com>
2021-02-08 00:52:47 -0700

    Enviar a todos los jefes

--- app/mailers/prospects_mailer.rb
+++ app/mailers/prospects_mailer.rb
@@ -2,11 +2,9 @@ class ProspectsMailer < ApplicationMailer
   def new_prospect(prospect)
     @prospect = prospect
     to_people = []
-    #Department.where(is_scientific: true).order(:name).each do |department|
-    #  to_people << department.person.email
-    #end
-
-    to_people << 'ion@cimav.edu.mx'
+    Department.where(is_scientific: true).order(:name).each do |department|
+     to_people << department.person.email
+    end
 
     mail(to: to_people, subject: "[SIP] Nuevo prospecto #{prospect.code}.")
   end
