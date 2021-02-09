22dccc0a4b360c147ed8e1863eb81f3be3a62c77
Jonathan Hernández <jonathan.hernandez@gmail.com>
2021-02-08 23:18:09 -0700

    Usar correo de desarrollo y TAG que muestra la versión en la que se está

--- app/mailers/prospects_mailer.rb
+++ app/mailers/prospects_mailer.rb
@@ -2,8 +2,12 @@ class ProspectsMailer < ApplicationMailer
   def new_prospect(prospect)
     @prospect = prospect
     to_people = []
-    Department.where(is_scientific: true).order(:name).each do |department|
-     to_people << department.person.email
+    if Rails.env.production? 
+      Department.where(is_scientific: true).order(:name).each do |department|
+        to_people << department.person.email
+      end
+    else 
+      to_people << ENV['EMAIL_DEVELOPER']
     end
 
     mail(to: to_people, subject: "[SIP] Nuevo prospecto #{prospect.code}.")
